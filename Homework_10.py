bdacf0982b8534cab2254be2b345e53ade9b8f04
AlexeyChernyshkov <alchern1999@gmail.com>
2023-10-22 19:48:28 +0700

    Домашняя работа №10

--- Homework_10.py
+++ Homework_10.py
@@ -1,4 +1,3 @@
-'''
 #------------------Задание 1--------------------------
 # Найдите количество различных элементов данного массива
 n = [4, -17, 3, -5, 16, 0, 36.2, 34, -2, -76, 4, 0, -2, 8, 81, 100, 25, 0, 0, 0, 0]
@@ -56,30 +55,116 @@ print(n)
 
 #------------------Задание 6--------------------------
 # напечатать прямоугольник (заданы длина, ширина и символ) при помощи цикла for
-h, l, symbol = 6, 7, "*"
-
-for i in range(h):
-    print(symbol * l)
-
-#------------------Задание 7--------------------------
-# напечатать полый прямоугольник (заданы длина, ширина и символ) при помощи цикла for
 
 h, l, symbol = 6, 7, "*"
-
+a = []
 for i in range(h):
-    if 0 < i < h-1:
-        print(symbol + " " * (l - 2) + symbol)
-    else:
-        print(symbol * l)
+    a.append([])
+    for j in range(l):
+        a[i].append(symbol)
 
-'''
+for row in a:
+    print()
+    for element in row:
+        print(element, end=' ')
+
 #------------------Задание 7--------------------------
+# напечатать прямоугольник (заданы длина, ширина и символ) при помощи цикла for
+
+h, l, symbol = 6, 7, "*"
+a = []
+for i in range(h):
+    a.append([])
+    for j in range(l):
+        if i != 0 and i != h-1:
+            if j == 0 or j == l-1:
+                a[i].append(symbol)
+            else:
+                a[i].append(" ")
+        else:
+            a[i].append(symbol)
+
+for row in a:
+    print()
+    for element in row:
+        print(element, end=' ')
 
 
 
 
+#------------------Задание 8--------------------------
+# напечатать квадрат (заданы длина и символ) при помощи цикла for по образцу
+# усложнение (рассматривать как отдельную задачу): программа не должна дважды проходить по уже пройденным элементам матрицы
+
+# Если при выводе массива проход тоже считается, тогда это не усложненная версия задачи
+
+l, symbol = 11, "*"
+if l % 2 == 0:
+    l += 1
+
+a = []
+for i in range(l):
+    a.append([])
+    for j in range(l):
+        if i != 0 and i != l-1 and j != 0 and j != l-1:
+            if (i % 2 != 0 and j % 2 != 0) or (i % 2 == 0 and j % 2 == 0):
+                a[i].append(''.join(symbol))
+            else:
+                a[i].append(''.join(" "))
+        else:
+            a[i].append(''.join(symbol))
+
+print('\n'.join(map(' '.join, a)))
 
 
+
+#------------------Задание 9--------------------------
+# напечатать ромб (заданы длина, ширина и символ)  при помощи цикла for
+
+h, symbol = 20, "*"
+if h % 2 == 0:
+    h += 1
+
+a = []
+for i in range(h):
+    a.append([])
+    for j in range(h):
+        if i <= int(h / 2) and int(h / 2) - i <= j <= int(h / 2) + i:
+            a[i].append(symbol)
+        elif i > int(h / 2) and a[int(h/2)-(i-int(h/2))][j] == "*": #тут проверяем такую же строку но в верхней части ромба (последнюю с первой и так далее)
+            a[i].append(symbol)
+        else:
+            a[i].append(" ")
+
+for row in a:
+    print()
+    for element in row:
+        print(element, end=' ')
+
+
+
+#------------------Задание 10--------------------------
+#напечатать полый ромб (заданы длина, ширина и символ) при помощи цикла for
+
+h, symbol = 7, "*"
+if h % 2 == 0:
+    h += 1
+
+a = []
+for i in range(h):
+    a.append([])
+    for j in range(h):
+        if i <= int(h / 2) and (j == int(h / 2) - i or j == int(h / 2) + i):
+            a[i].append(symbol)
+        elif i > int(h / 2) and a[int(h/2)-(i-int(h/2))][j] == "*": #тут проверяем такую же строку но в верхней части ромба (последнюю с первой и так далее)
+            a[i].append(symbol)
+        else:
+            a[i].append(" ")
+
+for row in a:
+    print()
+    for element in row:
+        print(element, end=' ')
 
 
 
